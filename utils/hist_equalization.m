2891905e680a0e98411223a4e52f6b6f331c0bc7
billyjuliux <billyjulius01@gmail.com>
2022-09-17 19:34:56 +0700

    refactor, fix equalized hist return value

--- utils/hist_equalization.m
+++ utils/hist_equalization.m
@@ -8,34 +8,38 @@ function return_val = hist_equalization(img)
 
     if (nchannel == 1 || (nchannel == 3 && is_rgb_same(img)))
         % ------------- Grayscale --------------
-        [raw_hist, equalized_hist, equalized_img] = hist_equalization_per_channel(img(:,:,1));
+        equalized_img = hist_equalization_per_channel(img(:,:,1));
         
         % Show original histogram
+        raw_hist = get_frequency(img(:,:,1));
         figure;    
         bar(raw_hist);
+
+        figure;    
+        imhist(img(:,:,1));
     
         % Show equalized img
         figure;    
         imshow(equalized_img);
     
         % Show equalized histogram
+        equalized_hist = get_frequency(equalized_img);
         figure;    
         bar(equalized_hist);
 
+        figure;    
+        imhist(histeq(img(:,:,1)));
+
     else
         % ------------- RGB --------------
+        % Show original histogram
+        raw_hist = get_frequency(img);
+        figure;    
+        bar(raw_hist);
+
         equalized_imgs = cell(3,1);
         for i = 1:nchannel
-            [raw_hist, equalized_hist, equalized_img] = hist_equalization_per_channel(img(:,:,i));
-
-            % Show original histogram
-            figure;    
-            bar(raw_hist);
-
-            % Show equalized histogram
-            figure;    
-            bar(equalized_hist);
-
+            equalized_img = hist_equalization_per_channel(img(:,:,i));
             equalized_imgs{i} = equalized_img;
         end
 
@@ -43,5 +47,10 @@ function return_val = hist_equalization(img)
         combined_img = cat(3, equalized_imgs{1}, equalized_imgs{2}, equalized_imgs{3});
         figure;
         imshow(combined_img)
+
+        % Show equalized histogram
+        equalized_hist = get_frequency(combined_img);
+        figure;    
+        bar(equalized_hist);
     end
 end
