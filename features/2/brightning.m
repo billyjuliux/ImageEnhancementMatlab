9097e58c9353a435a5cdc7fe1403097246ad60aa
cjustinw <13519006@std.stei.itb.ac.id>
2022-09-20 03:21:55 +0700

    feat: add img enhancement ui

--- features/2/brightning.m
+++ features/2/brightning.m
@@ -1,4 +1,8 @@
-function enhanced_img = brightning(img, a, b)
+function [raw_hist, enhanced_hist, enhanced_img] = brightning(img, a, b)
+    raw_hist = get_frequency(img);
+
     % Calculate image brightning
     enhanced_img = a * img + b;
+
+    enhanced_hist = get_frequency(enhanced_img);
 end
--- features/2/power_transformation.m
+++ features/2/power_transformation.m
@@ -1,6 +1,10 @@
-function enhanced_img = power_transformation(img, c, y)
+function [raw_hist, enhanced_hist, enhanced_img] = power_transformation(img, c, y)
+    raw_hist = get_frequency(img);
+
     % Calculate power transformation
     enhanced_img = c * (im2double(img) .^ y) * 256;
     
     enhanced_img = cast(enhanced_img, 'uint8');
+
+    enhanced_hist = get_frequency(enhanced_img);
 end
